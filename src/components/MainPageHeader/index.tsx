3f1d501c5e0733211994a7304483a398700c5224
samirsouzasys <samir.guitar@gmail.com>
2023-10-06 11:02:38 -0300

    feat: add MainPageHeader component

---
+++ src/components/MainPageHeader/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import './styles.css'
+
+import searchIcon from './assets/search.svg'
+
+const MainPageHeader = () => {
+    return(
+        <div className='container'>
+            <h1 className='title'>Busca de personagens</h1>
+            <label className='labelSearch'>Nome do personagem</label>
+            <div className='divSearch'>
+                <input className='inputSearch' placeholder="Search"/>
+                <Image className='iconSearch'src={searchIcon} alt="search icon"/>
+            </div>
+        </div>
+    )
+}
+
+export default MainPageHeader
