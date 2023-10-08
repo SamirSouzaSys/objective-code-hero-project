8739a0e3084dbdbd35bda4dc5141234bd0b7f6f8
samirsouzasys <samir.guitar@gmail.com>
2023-10-07 22:42:15 -0300

    feat: authorize communication with image provider

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns:[
+            {
+                hostname: "i.annihil.us",
+                protocol: 'http',
+                port: '',
+                pathname:'/**'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
